3def26cb75915c770bd578f7479d9375b81bc880
Maujin111 <maurial593@gmail.com>
2021-09-13 03:50:34 -0500

    fix

---
+++ js/controllers/aboutCtrl.js
@@ -0,0 +1,10 @@
+'use strict';
+
+appControllers.controller(
+    'about_ctrl',
+    ['$scope', '$rootScope', '$routeParams', '$location',
+    function ($scope, $rootScope, $routeParams, $location) {
+
+
+    }
+]);
---
+++ js/controllers/resourcesCtrl.js
@@ -0,0 +1,10 @@
+'use strict';
+
+appControllers.controller(
+    'resources_ctrl',
+    ['$scope', '$rootScope', '$routeParams', '$location',
+    function ($scope, $rootScope, $routeParams, $location) {
+
+
+    }
+]);
